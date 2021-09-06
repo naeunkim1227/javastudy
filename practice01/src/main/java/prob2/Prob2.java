d557afa499bbf471bfcfc1ba9edded8ba9027600
user <user@218.159.169.206>
2021-09-06 18:47:01 +0900

    21.09.06 first commit

---
+++ practice01/src/main/java/prob2/Prob2.java
@@ -0,0 +1,17 @@
+package prob2;
+
+public class Prob2 {
+	public static void main(String[] args) {
+		/* 코드 작성 */
+		for(int i=1;i<20;i++) {
+			for(int j=1;j<11;j++) {
+				System.out.print(j);
+				
+			}
+			
+			System.out.println("");
+			
+		}
+		
+	}
+}
