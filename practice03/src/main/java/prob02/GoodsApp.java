31f6a358befb4012f2ffc2b3282b68f0e3538a0c
naeun <kne0140@daum.net>
2021-09-10 00:28:27 +0900

    21.09.10 day 4

--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -1,5 +1,6 @@
 package prob02;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GoodsApp {
@@ -7,12 +8,21 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
+		System.out.println("입력하세요");
+		
+		String line = scanner.nextLine();
+		
+		String[] datas = line.split(" ");
+		
 		Goods[] goods = new Goods[COUNT_GOODS];
 
 		// 상품 입력
-
-		// 상품 출
+		Goods good = new Goods();
+		
+			for(String data: datas) {
+				System.out.print(data);
+			}
+		// 상품 출력
 		
 		// 자원정리
 		scanner.close();
