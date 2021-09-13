aacc90c4cdac2780e645b99a877d4f732a8fa100
naeunkim1227 <kne0140@daum.net>
2021-09-13 14:10:44 +0900

    21.09.13 day5

---
+++ chapter04/src/main/java/chapter04/StringTest04.java
@@ -0,0 +1,9 @@
+package chapter04;
+
+public class StringTest04 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ chapter04/src/main/java/collection/StackTest.java
@@ -0,0 +1,25 @@
+package collection;
+
+import java.util.Stack;
+
+public class StackTest {
+
+	public static void main(String[] args) {
+		Stack<String> s = new Stack<>();
+		
+		s.push("둘리");
+		s.push("마이콜");
+		s.push("도우너");
+		
+		while(!s.isEmpty()) {
+			String str = s.pop();
+			System.out.println(str);
+		}
+		
+		//비어있는 경우에는 예외발생
+		s.pop();
+		
+		
+	}
+
+}
