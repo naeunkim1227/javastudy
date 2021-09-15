7317460a3a342135f7f2ed5059251adb22d3c609
naeunkim1227 <kne0140@daum.net>
2021-09-15 17:02:59 +0900

    21.09.15 day8

--- network/src/main/java/thread/DigitThread.java
+++ network/src/main/java/thread/DigitThread.java
@@ -6,7 +6,12 @@ public class DigitThread extends Thread {
 	public void run() {
 		for(int i=0;i<10;i++) {
 			System.out.print(i);
-		
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
--- network/src/main/java/thread/UpperCaseAlphabet.java
+++ network/src/main/java/thread/UpperCaseAlphabet.java
@@ -1,20 +1,14 @@
 package thread;
 
-public class AlphabetThread extends Thread {
-
-	@Override
-	public void run() {
-		for(char c='a';c<='z';c++) {
+public class UpperCaseAlphabet {
+	public void print(){
+		for(char c='A';c<='Z';c++) {
 			System.out.print(c);
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-
-
-
 }
