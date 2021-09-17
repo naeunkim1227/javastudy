f09e41cdca68ce290c42a113c5533e2cce56b08a
naeun <kne0140@daum.net>
2021-09-17 14:07:21 +0900

    21.09.17

--- network/src/main/java/chat/gui/ChatWindow.java
+++ network/src/main/java/chat/gui/ChatWindow.java
@@ -29,8 +29,11 @@ public class ChatWindow {
 		textField = new TextField();
 		textArea = new TextArea(30, 80);
 	}
-
+	
 	public void show() {
+	
+		/* 1. UI 초기화 */
+		
 		// Button
 		buttonSend.setBackground(Color.GRAY);
 		buttonSend.setForeground(Color.WHITE);
@@ -39,17 +42,29 @@ public class ChatWindow {
 		//buttonSend.addActionListener((e) ->System.out.println("click"));
 		
 		
-		/*
 		buttonSend.addActionListener( new ActionListener() {
 			@Override
 			public void actionPerformed( ActionEvent actionEvent ) {
-				System.out.println("Click!!!");
 				sendMessage();
 			}
 		});
-		*/
+		
 		// Textfield
 		textField.setColumns(80);
+		textField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keycode = e.getKeyChar();
+				if(keycode == KeyEvent.VK_ENTER) {
+					//엔터키를 눌렀을때 샌드메세지 함수 실행 
+					sendMessage();
+				}
+			}
+			
+		
+		});
+		
 
 		// Pannel
 		pannel.setBackground(Color.LIGHT_GRAY);
@@ -69,9 +84,51 @@ public class ChatWindow {
 		});
 		frame.setVisible(true);
 		frame.pack();
+		
+		
+		//2.IOStream 가져오기
+		
+		//3.Chat Client Thread 생성
+		/* 스레드를 내부 클래스로 만들어 놔야 좋다.
+			
+		
+		*/
+		
+		
 	}
 	
 	private void sendMessage() {
-		System.out.println("send message");
+		String message = textField.getText();
+		System.out.println("메세지 보내는 프로토콜 구현:" + message);
+		textField.setText("");
+		textField.requestFocus();
+		
+		
+		//Receive Thread에서 서버로 부터 받은 메세지가 있다고 치고
+		updateTextArea("마이콜: " + message);
+		//가짜 데이터
 	}
+	
+	
+	private void updateTextArea(String message) {
+		textArea.append(message);
+		textArea.append("\n");
+	}
+	
+	
+	private void finish() {
+		System.out.println("소켓 닫기 or 방 나가기 프로토콜 구현");
+		System.exit(0);
+	}
+	
+	
+	private class ChatClientThread extends Thread{
+		//부모 메소드 접근에 편하기 때문에 이너클래스로 만들어 주자
+		public void run() {
+			updateTextArea("......");
+		}
+		
+	}
+	
+	
 }
